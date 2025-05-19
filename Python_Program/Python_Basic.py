dd843613c9f422b89081e3c2a60910ab97315845
Naagraaz <krishnamurari93km@gmail.com>
2025-05-20 00:51:06 +0530

    Program

--- Python_Program/Python_Basic.py
+++ Python_Program/Python_Basic.py
@@ -86,6 +86,163 @@ Num = int(input("Enter First number: "))
 Num1= int(input("Enter Second number: "))
 Num2=  int(input("Enter Third number: "))
 
+# 8. WAP to solve the given math formula.
+# Formula : (a + b)2 = a^2 + b^2 + 2ab
+
+A=int(input("Enter a number:"))
+B=int(input("Enter a number:"))
+
+Result=A**2 + B**2 +2*(A*B)
+
+
+print("The value of ((A+B)**2) is :",Result)
+
+LHS=((A+B)**2)
+
+RHS=A**2 + B**2 +2*(A*B)
+
+if LHS==RHS:
+    print("Correct Program:")
+else:
+    print("Wrong Program:")
+
+print("="*50)
+
+# 9. WAP  to solve the given math formula.
+# Formula : (a – b)2 = a^2 + b^2 – 2ab
+
+A=int(input("Enter a number:"))
+B=int(input("Enter a number:"))
+
+Result=A**2 + B**2 -2*(A*B)
+
+
+print("The value of ((A+B)**2) is :",Result)
+
+LHS=((A-B)**2)
+
+RHS=A**2 + B**2 -2*(A*B)
+
+if LHS==RHS:
+    print("Correct Program:")
+else:
+    print("Wrong Program:")
+
+print("="*50)
+
+# 10. WAP  to solve the given math formula.
+# Formula : a2 – b2 = (a-b)(a+b)
+
+A=int(input("Enter a number:"))
+B=int(input("Enter a number:"))
+
+Result=(A-B)*(A+B)
+
+
+print("The value of (A)**2-(B)**2) is :",Result)
+
+LHS=((A-B)**2)
+
+RHS=A**2 + B**2 -2*(A*B)
+
+if LHS==RHS:
+    print("Correct Program:")
+else:
+    print("Wrong Program:")
+
+print("="*50)
+
+# 11. WAP to solve the given math formula.
+# Formula : (a + b)3 = a3 + 3ab(a+b) + b3 
+
+A=int(input("Enter a number:"))
+B=int(input("Enter a number:"))
+
+Result=A**3 + B**3 +3*A*B*(A+B)
+
+
+print("The value of (a + b)3 is :",Result)
+
+LHS=(A+B)**3
+
+RHS=A**3 + B**3 +3*A*B*(A+B)
+
+if LHS==RHS:
+    print("Correct Program:")
+else:
+    print("Wrong Program:")
+
+print("="*50)
+
+# 12. WAP to solve the given math formula.
+# Formula : (a – b)3 = a3 – 3a2b + 3ab2 – b3
+
+A=int(input("Enter a number:"))
+B=int(input("Enter a number:"))
+
+# Find the Value of=(A-B)**3
+# (A – B)3 = A3 – 3A2B + 3AB2 – B3
+
+Result= A**3-3*A**2*B+3*A*B**2-B**3
+
+print("The value of (A-B)3 is :",Result)
+
+LHS=(A-B)**3
+
+RHS=A**3-3*A**2*B+3*A*B**2-B**3
+
+if LHS==RHS:
+    print("Correct Program:")
+else:
+    print("Wrong Program:")
+
+print("="*50)
+
+# 13. WAP to calculate the area of the square.
+#Formula : Area = a*a
+
+Side=int(input("Enter the side length of Square:"))
+
+Area=(Side)**2
+
+print("Area of square:",Area)
+
+print("="*50)
+
+# 14. WAP to calculate the area of a circle.
+
+# Formula = PI*r*r
+
+Radius = int(input("Enter the radius of Circle: "))
+
+PI = 3.14
+
+Area = PI * Radius**2
+
+print("Area of circle:", Area)
+
+# 15. WAP to calculate the area of a cube.
+# Formula = 6*a*a
+
+Side=int(input("Enter the side of cube:"))
+
+Area=6*Side**2
+print("Area of cube:",Area)
+
+# 16. WAp to find the area of cylinder.
+
+# Formula= 2*PI*r*h + 2*PI*r*r
+
+R=int(input("Enter the Radius of cylinder:"))
+H=int(input("Enter the Height of cylinder:"))
+PI=3.14
+Area=2*PI*R*H + 2*PI*R**2
+print("Area of Cylinder:",Area)
+
+# 17. WAP  to check whether the enter number is an Armstrong number or not.
+# Example: 153 = 1*1*1 + 5*5*5 + 3*3*3
+
+Number=int(input("Enter a Number:"))
 
 
 
